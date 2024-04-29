ae52cded5d7ee9a99b03552911cdca54bc2e6c3b
Robson Alves <bobpitaia@gmail.com>
2024-04-29 15:30:51 -0300

    Bootstrap added

--- market.py
+++ market.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World but bigger</h1>'
+def home_page():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
